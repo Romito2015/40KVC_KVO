87db7fbfcd1baaedeb8f690579bc8ca3e9def902
Roman <romachopovenko@Romas-Mac.local>
2015-10-23 13:00:16 +0300

    Initial Commit

---
+++ 40KVCHW/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  40KVCHW
+//
+//  Created by Roma Chopovenko on 10/23/15.
+//  Copyright (c) 2015 RSChopovenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RSAppDelegate class]));
+    }
+}
